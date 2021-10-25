391b01057abe6d9f66e7d5e0e2f007508abde09a
JoeyParisi1 <89203401+JoeyParisi1@users.noreply.github.com>
2021-10-25 11:59:04 -0400

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  alert("TRY AGAIN");
+}
+<!-- Correct fucntion for the link to the next puzzle-!>
+function myFunction2() {
+  alert("Link to next puzzle= https://codepen.io/Joeyparisi/pen/NWgeKKZ");
+}
