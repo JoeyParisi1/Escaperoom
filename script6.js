dc78c80fef6d0cd9829ea8e484e04d213d7002dd
JoeyParisi1 <89203401+JoeyParisi1@users.noreply.github.com>
2021-10-27 11:56:11 -0400

    Update script6.js

--- script6.js
+++ script6.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 $("[id=digit]").click(function() {
  $(".lock").addClass($(this).attr('class'));
     
-$(".lock.6.3.9").css("background", "green").html("Next Link=https://github.com/JoeyParisi1/Escaperoom/Congrats.html");
+$(".lock.6.3.9").css("background", "green").html("Next Link=https://joeyparisi1.github.io/Escaperoom/Congrats.html");
   });  
 })
